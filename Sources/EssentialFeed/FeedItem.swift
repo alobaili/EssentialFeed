abde541dea1548ff2af694e71ed3d0878db7224f
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2024-02-06 21:32:04 +0300

    Create the `FeedItem` model

---
+++ Sources/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//
+//
+//  Created by Abdulaziz Alobaili on 06/02/2024.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}

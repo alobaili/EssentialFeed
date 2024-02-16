a0632899e462dbfcbfb2355d7c7e4df827267ad9
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2024-02-16 16:42:00 +0300

    Add end-to-end API tests in a separate test target

--- Package.swift
+++ Package.swift
@@ -14,10 +14,14 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "EssentialFeed"),
+        .target(name: "EssentialFeed"),
         .testTarget(
-            name: "RemoteFeedLoaderTests",
-            dependencies: ["EssentialFeed"]),
+            name: "EssentialFeedTests",
+            dependencies: ["EssentialFeed"]
+        ),
+        .testTarget(
+            name: "EssentialFeedAPIEndToEndTests",
+            dependencies: ["EssentialFeed"]
+        )
     ]
 )

402f064d5877ccf9baaf97d424891e14a2e66f03
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2024-02-23 20:35:39 +0300

    Move `FeedStore` to its own file

---
+++ Sources/EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  
+//
+//  Created by Abdulaziz Alobaili on 23/02/2024.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}

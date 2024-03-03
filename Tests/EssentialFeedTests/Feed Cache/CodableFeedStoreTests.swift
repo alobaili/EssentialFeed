5578c84b6613253171ac66a95128b16988112830
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2024-03-03 20:54:50 +0300

    Extract duplicate insert test code into a reusable helper method

--- Tests/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ Tests/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -96,15 +96,7 @@ final class CodableFeedStoreTests: XCTestCase {
         let feed = uniqueImageFeed().local
         let timestamp = Date.now
 
-        let expectation = expectation(description: "Wait for cache retrieval")
-
-        sut.insert(feed, timestamp: timestamp) { insertionError in
-            XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
-
-            expectation.fulfill()
-        }
-
-        wait(for: [expectation], timeout: 1)
+        insert((feed, timestamp), to: sut)
 
         expect(sut, toRetrieve: .found(feed: feed, timestamp: timestamp))
     }
@@ -114,14 +106,7 @@ final class CodableFeedStoreTests: XCTestCase {
         let feed = uniqueImageFeed().local
         let timestamp = Date.now
 
-        let expectation = expectation(description: "Wait for cache insertion")
-
-        sut.insert(feed, timestamp: timestamp) { insertionError in
-            XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
-            expectation.fulfill()
-        }
-
-        wait(for: [expectation], timeout: 1)
+        insert((feed, timestamp), to: sut)
 
         expect(sut, toRetrieveTwice: .found(feed: feed, timestamp: timestamp))
     }
@@ -133,6 +118,17 @@ final class CodableFeedStoreTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
 
         return sut
+    }
+
+    private func insert(_ cache: (feed: [LocalFeedImage], timestamp: Date), to sut: CodableFeedStore) {
+        let expectation = expectation(description: "Wait for cache insertion")
+
+        sut.insert(cache.feed, timestamp: cache.timestamp) { insertionError in
+            XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 1)
     }
 
     private func expect(

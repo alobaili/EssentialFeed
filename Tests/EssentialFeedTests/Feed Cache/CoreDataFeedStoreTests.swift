5dce28a3ce982d94d8ca760754402129768b03ee
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2024-06-03 21:24:26 +0300

    Add empty CoreData feed store specs

--- Tests/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ Tests/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -1,8 +1,58 @@
 //
-//  File.swift
-//  
+//  CoreDataFeedStoreTests.swift
+//
 //
 //  Created by Abdulaziz Alobaili on 03/06/2024.
 //
 
-import Foundation
+import XCTest
+
+class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+
+    }
+    
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+
+    }
+    
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+
+    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+
+    }
+    
+    func test_storeSideEffects_runSerially() {
+
+    }
+}
